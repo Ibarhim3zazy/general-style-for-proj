4a1be1760736b7663809b777c77a38979e9fef39
Ibrahim 3zazy <83598736+Ibarhim3zazy@users.noreply.github.com>
2021-10-06 12:12:10 +0200

    Add files via upload

---
+++ e_store_for_course/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/general.css">
+  </head>
+  <body>
+    <header>
+      <a href="#" style="color:#fff;border-bottom: #fff solid 6px;margin:20px;">Home</a>
+      <a href="#">about</a>
+    </header>
+    <section>
+      <h1>button color</h1>
+      <input type="button" class="color" value="button color">
+      <input type="button" class="color" value="button color 2">
+      <input type="button" class="color" value="button color 3">
+      <input type="button" class="color" value="button color 4">
+      <input type="button" class="color" value="button color 5">
+      <input type="button" class="color" value="button color 6">
+    </section>
+    <section>
+      <h1>button size</h1>
+      <input type="button" class="buton_size_small" value="small size">
+      <input type="button" class="buton_size_mediam" value="mediam size">
+      <input type="button" class="buton_size_mediam2" value="mediam2 size">
+      <input type="button" class="buton_size_large" value="large size">
+    </section>
+    <section>
+      <h1>links size and color</h1>
+      <a href="#" id="link1">body Link 1</a><br><br>
+      <a href="#" id="link2">body Link 2</a><br><br>
+      <a href="#" id="link4">body Link 4 &#10095;</a><br><br>
+      <a href="#" id="link5">&#10094; body Link 5</a><br><br>
+    </section>
+    <section>
+      <h1>text color</h1>
+      <input type="text" placeholder="text color">
+    </section>
+    <section>
+      <h1>text size</h1>
+      <input type="text" class="text_size_small" placeholder="small size">
+      <input type="text" class="text_size_mediam" placeholder="mediam size">
+      <input type="text" class="text_size_mediam2" placeholder="mediam2 size">
+      <input type="text" class="text_size_large" placeholder="large size">
+    </section>
+
+    <footer>
+      <a href="#" id="link3">footer Link 3</a><br><br>
+      <a href="#" id="link6">spesific link</a>
+    </footer>
+  </body>
+</html>
